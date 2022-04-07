39b779920502f313e73915fcddd67b8eea18767e
Tomas Uzcudun <tomas.uzcudun@students.salle.url.edu>
2022-04-07 21:34:12 +0200

    v1.1 Login Layout
    [+] New sprites for the login layout:
        - Email icon
        - Lock icon
    [+] Adding new sprites to the UI.
    [+] Adding margins and paddings between panels.
    [+] Login layout GUI (Almost finished).

--- src/LoginView.java
+++ src/LoginView.java
@@ -5,22 +5,12 @@ import java.io.File;
 import java.io.IOException;
 
 public class LoginView extends JFrame {
+
     public LoginView() {
-        setSize(1280, 720);
 
-        setTitle("Battleship v1.0.0");
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        initializeWindow();
 
-        Font font = null;
-
-        try {
-            font = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Poppins-Bold.ttf")).deriveFont(20f);
-            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Poppins-Bold.ttf")));
-        } catch (FontFormatException | IOException e) {
-            e.printStackTrace();
-        }
+        Font font = initializeFont();
 
         // ------------------------ Background Image ------------------------ //
 
@@ -32,70 +22,124 @@ public class LoginView extends JFrame {
 
         // ------------------------ Login Button ------------------------ //
 
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BorderLayout());
+        buttonPanel.setPreferredSize(new Dimension(200,100));
+        buttonPanel.setBackground(Color.red);
+
         JImagePanel button = new JImagePanel("sprites/login_button.png");
-        button.setPreferredSize(new Dimension(200,100));
+        button.setPreferredSize(new Dimension(100,100));
         button.setOpaque(false);
 
-        // ------------------------ Email EditText ------------------------ //
+        buttonPanel.add(button);
+
+        // ------------------------ Email Box Layout ------------------------ //
+
+        JPanel emailPanel = new JPanel();
+        emailPanel.setLayout(new BoxLayout(emailPanel, BoxLayout.X_AXIS));
+        emailPanel.setPreferredSize(new Dimension(350, 25));
+        emailPanel.setOpaque(false);
+
+        JImagePanel emailIco = new JImagePanel("sprites/email_ico.png");
+        emailIco.setOpaque(false);
+        emailIco.setPreferredSize(new Dimension(35,0));
 
         JTextField emailInput = new JTextField(10);
         emailInput.setOpaque(false);
         emailInput.setFont(font);
         emailInput.setForeground(new Color(255,255,255));
-        emailInput.setBorder(BorderFactory.createEmptyBorder(20, 20, 0, 20));
+        emailInput.setBorder(BorderFactory.createEmptyBorder(0, 5, 5, 20));
 
         Border oldBorder = emailInput.getBorder();
         Border whiteBorder = BorderFactory.createMatteBorder(0, 0, 3, 0, Color.WHITE);
         Border newBorder = BorderFactory.createCompoundBorder(whiteBorder, oldBorder);
         emailInput.setBorder(newBorder);
 
-        /*TextPrompt placeholder = new TextPrompt("Apellido Paterno", textField);
-        placeholder.changeAlpha(0.75f);
-        placeholder.changeStyle(Font.ITALIC);
-        JOptionPane.showMessageDialog(null, textField);*/
+        emailPanel.add(emailIco);
+        emailPanel.add(addSeparator(20,0));
+        emailPanel.add(emailInput);
 
-        //emailInput.setPreferredSize(new Dimension(350,50));
+        // ------------------------ Password Box Layout ------------------------ //
 
-        // ------------------------ Password EditText ------------------------ //
+        JPanel passwordPanel = new JPanel();
+        passwordPanel.setLayout(new BoxLayout(passwordPanel, BoxLayout.X_AXIS));
+        passwordPanel.setPreferredSize(new Dimension(350, 35));
+        passwordPanel.setOpaque(false);
+
+        JImagePanel passwordIco = new JImagePanel("sprites/lock_ico.png");
+        passwordIco.setOpaque(false);
+        passwordIco.setPreferredSize(new Dimension(35,0));
 
         JTextField passwordInput = new JTextField();
         passwordInput.setOpaque(false);
         passwordInput.setFont(font);
         passwordInput.setForeground(new Color(255,255,255));
-        passwordInput.setBorder(BorderFactory.createEmptyBorder(20, 20, 0, 20));
+        passwordInput.setBorder(BorderFactory.createEmptyBorder(0, 5, 5, 20));
 
         Border oldBorder2 = passwordInput.getBorder();
         Border whiteBorder2 = BorderFactory.createMatteBorder(0, 0, 3, 0, Color.WHITE);
         Border newBorder2 = BorderFactory.createCompoundBorder(whiteBorder2, oldBorder2);
         passwordInput.setBorder(newBorder2);
 
-        //passwordInput.setPreferredSize(new Dimension(350,50));
+        passwordPanel.add(passwordIco);
+        passwordPanel.add(addSeparator(20,0));
+        passwordPanel.add(passwordInput);
 
         // ------------------------ Display Objects ------------------------ //
 
         gbc.gridx = 0; gbc.gridy = 0;
-        backgroundPanel.add(emailInput, gbc);
+        backgroundPanel.add(addSeparator(0,200), gbc);
 
         gbc.gridx = 0; gbc.gridy = 1;
-        backgroundPanel.add(passwordInput, gbc);
-
-        // Space between button and text field
-
-        JPanel space = new JPanel();
-        space.setLayout(new BoxLayout(space, BoxLayout.Y_AXIS));
-        space.setOpaque(false);
-
-        Component rigidArea = Box. createRigidArea(new Dimension(100, 100));
-        space.add(rigidArea);
+        backgroundPanel.add(emailPanel, gbc);
 
         gbc.gridx = 0; gbc.gridy = 2;
-        backgroundPanel.add(space, gbc);
+        backgroundPanel.add(addSeparator(0,50), gbc);
 
         gbc.gridx = 0; gbc.gridy = 3;
-        backgroundPanel.add(button, gbc);
+        backgroundPanel.add(passwordPanel, gbc);
+
+        gbc.gridx = 0; gbc.gridy = 4;
+        backgroundPanel.add(addSeparator(0,100), gbc);
+
+        gbc.gridx = 0; gbc.gridy = 5;
+        backgroundPanel.add(buttonPanel, gbc);
 
         add(backgroundPanel);
 
         setVisible(true);
+    }
+
+    public void initializeWindow () {
+        setSize(1280, 720);
+
+        setTitle("Battleship v1.0.0");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public Font initializeFont () {
+        Font font = null;
+
+        try {
+            font = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Poppins-Bold.ttf")).deriveFont(20f);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Poppins-Bold.ttf")));
+        } catch (FontFormatException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return font;
+    }
+
+    public JPanel addSeparator (int width, int height) {
+        JPanel space = new JPanel();
+        space.setLayout(new BoxLayout(space, BoxLayout.Y_AXIS));
+        space.setOpaque(false);
+
+        Component rigidArea = Box. createRigidArea(new Dimension(width, height));
+        space.add(rigidArea);
+
+        return space;
     }
 }
