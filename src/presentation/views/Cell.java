46866d80379f03722680e879311c6f4dd8caf1f6
Esteban Ariel Gómez Agüero <estebanariel.gomez@students.salle.url.edu>
2022-05-16 19:50:45 +0200

    Quitada una propiedad no utilizada en la clase Cell

--- src/presentation/views/Cell.java
+++ src/presentation/views/Cell.java
@@ -4,21 +4,11 @@ public class Cell extends JImagePanel {
 
     private final int x;
     private final int y;
-    private boolean isOccupied;
 
     public Cell (int x, int y, String img) {
         super(img);
         this.x = x;
         this.y = y;
-        this.isOccupied = false;
-    }
-
-    public boolean isOccupied() {
-        return isOccupied;
-    }
-
-    public void setOccupied(boolean occupied) {
-        isOccupied = occupied;
     }
 
     public int[] getCoordinates() {
